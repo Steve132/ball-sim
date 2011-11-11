7d7ae54fedf7b3b071519b00637e07749078648d
nlarnold@gmail.com <nlarnold@gmail.com@fafa6a57-ed7c-ac1b-a53c-de6559e86a30>
2011-11-11 15:30:13 +0000

    Added data parser file (in Python!), as well as a copy of the file that it outputs.

---
+++ trunk/new_data_parser.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+import sys
+
+f = open('headless_data.txt', 'r')
+out = open('headless_data.csv', 'w')
+out.write("Threads\tScale\tNumSpheres\tTimesteps\tTotal Time\tTotal Collisions\tWall Collisions\tSphere Collisions\t")
+out.write("Total Checks\tAvg. Checks per Timestep\tAvg. Timesteps per Second\tAvg. Milliseconds per Timestep\t")
+out.write("Time Dilation\tTotal Threads")
+lineout = ""
+for li in f.readlines():
+	curlist = li.split()
+	for items in curlist:
+        # if the first item in the list is 'Threads', this is a new line
+		if items == 'Threads':
+			lineout += "\n"
+			out.write(lineout)
+			lineout = ""
+		elif items.isdigit():
+			lineout += items+"\t"
+		elif items.rfind('.') != -1:
+			lineout += items+"\t"
+out.write(lineout)
+f.close()
+out.close()
