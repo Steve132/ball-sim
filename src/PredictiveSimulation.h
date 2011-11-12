76f68e41698fd0d7f2bace6b3d53d5a7635cc28f
badgerboy132@gmail.com <badgerboy132@gmail.com@fafa6a57-ed7c-ac1b-a53c-de6559e86a30>
2011-11-12 01:12:07 +0000

    Untested Draft of full prediction system.

--- src/PredictiveSimulation.h
+++ src/PredictiveSimulation.h
@@ -7,7 +7,30 @@ class PredictiveSimulation: public Simulation
 {
 public:
 	PredictiveSimulation(int,char**);
+	void sim_thread(unsigned int thread_id,std::uint64_t timesteps,barrier* b);
+
 protected:
+	class Collision
+	{
+	public:
+		double time;
+		bool valid;
+		Sphere* sphere1;
+		AxisPlane* wall;
+		Sphere* sphere2;
+		
+		Collision();
+		void invalidate();
+		bool verify();
+		void react();
+		bool operator<(const Collision& c);
+		
+	};
+	Collision repredict(Sphere* sh,double current_time);
+
+	std::vector<Collision> collisions;
+	std::vector<std::thread> threadpool;
+
 	virtual void spawn_sim_threads(std::uint64_t timesteps,barrier* bar);
 	virtual void join_sim_threads();
 	//virtual void update(double dt);
