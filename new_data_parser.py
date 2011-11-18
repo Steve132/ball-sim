72cd9800238dd20b6958336019063b6f6f9368bb
nlarnold@gmail.com <nlarnold@gmail.com@fafa6a57-ed7c-ac1b-a53c-de6559e86a30>
2011-11-18 15:49:49 +0000

    Added data files from script runs.  Also added new script files.  The Barrier.h mod was the removal of a #define comment at the beginning

--- new_data_parser.py
+++ new_data_parser.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 import sys
 
-f = open('headless_data.txt', 'r')
-out = open('headless_data.csv', 'w')
+f = open('headless_data_morespheres.txt', 'r')
+out = open('headless_data_morespheres.csv', 'w')
 out.write("Threads\tScale\tNumSpheres\tTimesteps\tTotal Time\tTotal Collisions\tWall Collisions\tSphere Collisions\t")
 out.write("Total Checks\tAvg. Checks per Timestep\tAvg. Timesteps per Second\tAvg. Milliseconds per Timestep\t")
 out.write("Time Dilation\tTotal Threads")
